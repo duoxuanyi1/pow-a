2c0c81fc7c3aaf2a9cf6d3c08dc7308419bf4bc3
HanWanghua <1348459234@qq.com>
2018-11-03 16:32:16 +0800

    串口通讯

---
+++ uart.c
@@ -0,0 +1,95 @@
+#include <STC12C5A60S2.H>
+#include "intrins.h"
+#include "uart.h"
+
+//sbit bit9 = P2^2;           //P2.2 show UART data bit9
+bit busy;
+
+
+void UartInit(void)
+{
+#if (PARITYBIT == NONE_PARITY)
+    SCON = 0x50;            //8-bit variable UART
+#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) || (PARITYBIT == MARK_PARITY)
+    SCON = 0xda;            //9-bit variable UART, parity bit initial to 1
+#elif (PARITYBIT == SPACE_PARITY)
+    SCON = 0xd2;            //9-bit variable UART, parity bit initial to 0
+#endif
+
+    TMOD |= 0x20;            //Set Timer1 as 8-bit auto reload mode
+    TH1 = TL1 = -(FOSC/12/32/BAUD); //Set auto-reload vaule
+    TR1 = 1;                //Timer1 start run
+    ES = 1;                 //Enable UART interrupt
+    EA = 1;                 //Open master interrupt switch
+
+    SendString("Traffic System\r\nUart Test !\r\nWork Start\r\n");
+}
+
+/*----------------------------
+UART interrupt service routine
+----------------------------*/
+void Uart_Isr() interrupt 4 using 1
+{
+    if (RI)
+    {
+        RI = 0;             //Clear receive interrupt flag
+        //P0 = SBUF;          //P0 show UART data
+        //bit9 = RB8;         //P2.2 show parity bit
+    }
+    if (TI)
+    {
+        TI = 0;             //Clear transmit interrupt flag
+        busy = 0;           //Clear transmit busy flag
+    }
+}
+
+/*----------------------------
+Send a byte data to UART
+Input: dat (data to be sent)
+Output:None
+----------------------------*/
+void SendData(BYTE dat)
+{
+    while (busy);           //Wait for the completion of the previous data is sent
+    ACC = dat;              //Calculate the even parity bit P (PSW.0)
+    if (P)                  //Set the parity bit according to P
+    {
+#if (PARITYBIT == ODD_PARITY)
+        TB8 = 0;            //Set parity bit to 0
+#elif (PARITYBIT == EVEN_PARITY)
+        TB8 = 1;            //Set parity bit to 1
+#endif
+    }
+    else
+    {
+#if (PARITYBIT == ODD_PARITY)
+        TB8 = 1;            //Set parity bit to 1
+#elif (PARITYBIT == EVEN_PARITY)
+        TB8 = 0;            //Set parity bit to 0
+#endif
+    }
+    busy = 1;
+    SBUF = ACC;             //Send data to UART buffer
+}
+
+/*----------------------------
+Send a string to UART
+Input: s (address of string)
+Output:None
+----------------------------*/
+void SendString(char *s)
+{
+    while (*s)              //Check the end of the string
+    {
+        SendData(*s++);     //Send current char and increment string ptr
+    }
+}
+
+void SendUInt(int x)
+{
+   char class[2];
+   class[0]	= x/10+'0';
+   class[1] = x%10+'0';
+   SendString(class);
+}
+
---
+++ uart.h
@@ -0,0 +1,24 @@
+#ifndef UART_H_
+#define UART_H_
+typedef unsigned char BYTE;
+typedef unsigned int WORD;
+
+#define FOSC 11059200L      //System frequency
+#define BAUD 9600           //UART baudrate
+
+/*Define UART parity mode*/
+#define NONE_PARITY     0   //None parity
+#define ODD_PARITY      1   //Odd parity
+#define EVEN_PARITY     2   //Even parity
+#define MARK_PARITY     3   //Mark parity
+#define SPACE_PARITY    4   //Space parity
+
+#define PARITYBIT EVEN_PARITY   //Testing even parity
+
+void UartInit(void);
+void SendData(BYTE dat);
+void SendString(char *s);
+void SendUInt(int x);
+
+
+#endif /*UART_H_*/
