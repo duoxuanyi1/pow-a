e9d01e31e8010312e40684cfc2d5ede43ced84a2
GavinloveGlary <1260326370@qq.com>
2018-11-03 16:51:49 +0800

    powa()测试通过

--- main.c
+++ main.c
@@ -1,13 +1,34 @@
-#include<stdio.h>
-#include<math.h>
-int sqrt(int n)
+#include <stdio.h>
+#include <stdlib.h> 
+int powa(int n)
 {
-	int m;
-	m=n*n;
+	int m ;
+	m = n*n;
 	return m;
 }
+void test_powa()
+{
+	int a;
+	a=1;
+	if(powa(a)!=1)
+		printf("error---a=1  m!=1");
+	else
+		printf("conform");
+	a=rand();
+	if(powa(a)!=a*a)
+		printf("error---a=k  m!=k*k");
+	else
+		printf("conform");
+	a++;
+	if(powa(a)!=a*a)
+		printf("error---a=k+1  m!=(k+1)*(k+1)");
+	else
+		printf("conform");
+ } 
+
 int main()
 {
-   printf("%d",sqrt(4));
+	//test_powa();//≤‚ ‘Õ®π˝ 
+   printf("%d",powa(4));
    return 0;
- } 
+} 
